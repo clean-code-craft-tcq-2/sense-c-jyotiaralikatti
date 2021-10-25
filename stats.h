d5b6c6f0302e26909775a495cdbc53d0b1676072
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2021-10-25 22:14:09 +0530

    Added function declaration and definitions

--- stats.h
+++ stats.h
@@ -1,8 +1,25 @@
+#include<stdbool.h>
 
+struct Stats
+{
+	float average;
+	float min;
+	float max;
+};
+
+#define NAN 0;// remove this
+const float maxThreshold = 10.2 ;
 struct Stats compute_statistics(const float* numberset, int setlength);
 
-typedef void (*alerter_funcptr)();
+typedef void (*alerter_funcptr)(void);
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
+bool emailAlerter(void);
+bool ledAlerter(void);
+
+//alerter_funcptr alerters[2] = {emailAlerter, ledAlerter};
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+
+
