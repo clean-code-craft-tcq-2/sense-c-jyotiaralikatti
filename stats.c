01365e05b5d925ddc64c3c12e54349ebc47a80a5
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2021-10-26 01:40:06 +0530

    Updated check_and_alert()

--- stats.c
+++ stats.c
@@ -2,15 +2,22 @@
 #include <stdio.h>
 
 /* global variables */
-//maxThreshold = 10.2;
+
 int emailAlertCallCount = 0 ;
 int ledAlertCallCount = 0;
 
+alerter_funcptr alerters[] = {emailAlerter, ledAlerter} ;
+
 int main()
 {
-    float numberset[] = {1.5, 8.9};// pointer to constant flaot
+    /* Enter different values of array elements or use scanf for the same*/
+    float numberset[] =  {99.8, 34.2, 4.5};//= {1.5, 8.9};// pointer to constant flaot
+    
     int i,setlength = sizeof(numberset) / sizeof(numberset[0]);
+    
     struct Stats computedStats = compute_statistics(numberset, setlength);
+    
+    check_and_alert(maxThreshold, alerters, computedStats);
     
     //Printing elements of computedStats
     printf("\n Elements of numberset ");
@@ -21,12 +28,41 @@ int main()
     printf("\n Elements of computedStats ");
     printf("\n %f \t %f \t %f \t ",computedStats.max,computedStats.min,computedStats.average);
     
+    //Printing check and alert status
+    if (emailAlertCallCount && ledAlertCallCount)
+    {/* max > Threshhold*/
+        printf("\n ALERT !!..emailAlertCallCount: %d \t ledAlertCallCount: %d", emailAlertCallCount,ledAlertCallCount);
+        emailAlertCallCount = 0;
+        ledAlertCallCount = 0;
+    }
+    else
+    printf("\n emailAlertCallCount: %d \t ledAlertCallCount: %d", emailAlertCallCount,ledAlertCallCount );
+    
+    
 }
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
-    
-    
+  
+  if(computedStats.max > maxThreshold)
+  {
+      /* Send alerts */
+      emailAlerter();
+      ledAlerter();
+  }
+  else
+  { /* Do nothing*/
+  }
+
+}
+
+void emailAlerter()
+{
+     emailAlertCallCount = 1;
+}
+void ledAlerter()
+{
+     ledAlertCallCount = 1;
 }
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
