1cce70185945508ef70bc7fb9d41fd942a87b30d
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2021-10-27 13:25:33 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,6 +1,11 @@
 #include "stats.h"
 #include "alerter.h"
 
+
+/* global variables */
+int emailAlertCallCount = 0 ;
+int ledAlertCallCount = 0;
+
 void emailAlerter()
 {
      emailAlertCallCount = 1;
