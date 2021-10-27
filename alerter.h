cbfeddb669dfd94ade3a01491a1b8cbebe86d007
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2021-10-27 13:27:21 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,5 +1,9 @@
     // create additional .c and .h files
     // containing the emailAlerter, ledAlerter functions
-    
+   
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
 void emailAlerter(void);
 void ledAlerter(void);   
