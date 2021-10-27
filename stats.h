c21bcedc7922a3e81fe9a9b59ff4ee830b8cde44
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2021-10-27 13:26:53 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -12,10 +12,5 @@ struct Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)(void);
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-void emailAlerter(void);
-void ledAlerter(void);
-
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
 
 
